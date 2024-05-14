5ffe839ca287a6fed6e12a70f2c3f31ea74f9ea3
allanrotiago <168598142+allanrotiago@users.noreply.github.com>
2024-05-14 18:54:18 -0300

    Adicionado uma nova branch de teste

---
+++ LocadoraVeiculo/Moto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculo
+{
+    public class Moto : Veiculos
+    {
+        public string TipoMotor { get; set; }
+        public int Cilindrada { get; set; }
+
+        public override decimal CalcularCustoLocacao(int dias)
+        {
+            decimal custoTotal = PrecoLocacaoDiaria * dias;
+            return custoTotal;
+        }
+    }
+}
